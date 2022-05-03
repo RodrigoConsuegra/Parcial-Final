082d6e9ae5234aa39665cc1cd12f7d6bc77faa4d
RodrigoMartinez <100334296+RodrigoConsuegra@users.noreply.github.com>
2022-05-03 09:28:29 -0600

    Datos de estudiante
    Se realizo el documento .py en el cual estan mis datos de estudiante

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Hola mi nombre es Jose Rodrigo Consuegra Martinez')
+print('Soy estudiante de primer años de la Universidad Rafael Landivar')
+print('Mi nombre de carne es 1577722')
+print('Soy estudiante de la carrera de ingenieria en informatica y sistemas')
